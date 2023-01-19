95a2049d1b982fa89bbb55a710356442f4982023
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-19 12:55:36 +0530

    [Bindhu]Add:Retrieve Employee Payroll from Database

--- EmployeePayrollAdoNet/Employee.cs
+++ EmployeePayrollAdoNet/Employee.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using EmployeePayrollAdoNet;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmployeeAdoNet
+namespace EmployeePayrollAdoNet
 {
     public class Employee
     {
+        public static string connectionString = @"data source = (localdb)\\\\MSSQLLocalDB; initial catalog = master; integrated security = true\";
+        SqlConnection sqlconnection = new SqlConnection(connectionString);
         public static void EmpDatabase()
         {
-            SqlConnection con = new SqlConnection("data source = (localdb)\\MSSQLLocalDB; initial catalog = master; integrated security = true");
             try
             {
                 string query = "Create Database EmployeePayroll";
@@ -27,6 +30,40 @@ namespace EmployeeAdoNet
             finally
             {
                 con.Close();
+            }
+        }
+        public int AddEmployee(EmployeeModel employeeModel)
+        {
+            try
+            {
+                using (this.sqlconnection)
+                {
+                    this.sqlconnection.Open();
+                    SqlCommand command = new SqlCommand("spAddEmployee", this.sqlconnection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Name", employeeModel.Name);
+                    command.Parameters.AddWithValue("@Salary", employeeModel.Salary);
+                    command.Parameters.AddWithValue("@Gender", employeeModel.Gender);
+                    command.Parameters.AddWithValue("@Address", employeeModel.Address);
+                    command.Parameters.AddWithValue("@PhoneNumber", employeeModel.PhoneNumber);
+                    command.Parameters.AddWithValue("@Department", employeeModel.Department);
+                    command.Parameters.AddWithValue("@BasicPay", employeeModel.BasicPay);
+                    command.Parameters.AddWithValue("@Deductions", employeeModel.Deductions);
+                    command.Parameters.AddWithValue("@TaxablePay", employeeModel.TaxablePay);
+                    command.Parameters.AddWithValue("@IncomeTax", employeeModel.IncomeTax);
+                    command.Parameters.AddWithValue("@NetPay", employeeModel.NetPay);
+                    int result = command.ExecuteNonQuery();
+                    this.sqlconnection.Close();
+                    if (result >= 1)
+                    {
+                        Console.WriteLine("Employee Added Successfully");
+                    }
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
             }
         }
     }
--- EmployeePayrollAdoNet/EmployeeModel.cs
+++ EmployeePayrollAdoNet/EmployeeModel.cs
@@ -8,18 +8,18 @@ namespace EmployeePayrollAdoNet
 {
     public class EmployeeModel
     {
-        public static int Id { get; set; }
-        public static string Name { get; set; }
-        public static long Salary { get; set; }
-        public static DateTime Start { get; set; }
-        public static char Gender { get; set; }
-        public static int PhoneNumber { get; set; }
-        public static string Address { get; set; }
-        public static string Department { get; set; }
-        public static long BasicPay { get; set; }
-        public static long Deductions { get; set; }
-        public static long TaxablePay { get; set; }
-        public static long IncomeTax { get; set; }
-        public static long NetPay { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Salary { get; set; }
+        public DateTime Start { get; set; }
+        public char Gender { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public long BasicPay { get; set; }
+        public long Deductions { get; set; }
+        public long TaxablePay { get; set; }
+        public long IncomeTax { get; set; }
+        public long NetPay { get; set; }
     }
 }
