1aa870362bbd7c2ff5f117ce2526741bb24bffbd
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-19 12:22:05 +0530

    [Bindhu]Add: Create EmployeePayroll Service Database

---
+++ EmployeePayrollAdoNet/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace EmployeeAdoNet
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("Welcome to AdoDotNet Database Program");
+                Console.WriteLine("1) Create Database\n");
+                int result = (int)Convert.ToInt64(Console.ReadLine());
+                switch (result)
+                {
+                    case 1:
+                        Employee.EmpDatabase();
+                        break;
+                }
+            }
+        }
+    }
+}
